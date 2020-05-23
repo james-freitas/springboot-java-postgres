86da2986505fde8da4bcc1e4b40fa97f7250ea55
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-05-23 19:17:44 -0300

    config: set up flyway

--- src/main/java/com/codeonblue/sample/domain/Category.java
+++ src/main/java/com/codeonblue/sample/domain/Category.java
@@ -5,9 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -19,8 +22,13 @@ import javax.persistence.Table;
 public class Category {
 
     @Id
-    @GeneratedValue
-    private Long id;
+    @SequenceGenerator(name="category_id_seq",
+            sequenceName="category_id_seq",
+            allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator="category_id_seq")
+    @Column(updatable = false)
+    private Integer id;
 
     private String description;
 
---
+++ src/main/resources/db/migration/V1__Create_category_sequence.sql
@@ -0,0 +1,10 @@
+-- public.category_id_seq definition
+
+-- DROP SEQUENCE public.category_id_seq;
+
+CREATE SEQUENCE public.category_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	START 1
+	CACHE 1
+	NO CYCLE;
